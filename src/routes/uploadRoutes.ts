8a3b4432dea4fea578bcc98ff0000b6a291aa9bc
DAVID <owolagbadavid@gmail.com>
2023-07-01 15:48:04 +0100

    s3 client

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,16 +1,38 @@
 import type { Express } from 'express';
 import keys from '../config/keys';
-import * as AWS from 'aws-sdk';
+import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import requireLogin from '../middlewares/requireLogin';
+// import * as AWS from 'aws-sdk';
 
-const s3 = new AWS.S3({
-	accessKeyId: keys.accessKeyId,
-	secretAccessKey: keys.secretAccessKey,
+// const s32 = new AWS.S3({
+// 	accessKeyId: keys.accessKeyId,
+// 	secretAccessKey: keys.secretAccessKey,
+// });
+const s3 = new S3({
+	credentials :{accessKeyId: keys.accessKeyId,
+		secretAccessKey: keys.secretAccessKey},
+	region: 'eu-north-1',                    
 });
+
 
 export default (app: Express) => {
 
-	app.get('/api/upload', (req, res) => {
-		res.send('hello');
+
+	app.get('/api/upload', requireLogin, async (req, res) => {
+
+		const key = `${req.user.id}/${uuidv4()}.jpeg`;
+
+		const command = new PutObjectCommand({
+			Bucket: 'nodejs-blog',
+			Key: key,
+			ContentType: 'jpeg',
+		});
+    
+
+		const url = await getSignedUrl(s3, command);
+		res.send({ key, url });
 	});
 
 };
